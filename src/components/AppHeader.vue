82e67cac1de5a6a3bd2a5f0b149be0df1a8e1414
Snehal Baghel <snehalbaghel@gmail.com>
2019-09-03 19:01:44 +0530

    adds toolbar and nav

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,59 @@
+<template>
+	<v-app id="toolbar">
+		<v-app-bar
+			app
+			clipped-left
+			color="green lighten-1">
+			<v-app-bar-nav-icon @click="drawer = !drawer" />
+			<span class="title ml-3 mr-5">E&nbsp;<span class="font-weight-light">Notice</span></span>
+			<v-text-field 
+				solo-inverted
+				flat
+				hide-details
+				label="Search"
+				prepend-inner-icon="search"
+			/>
+			<div class="flex-grow-1"></div>
+		</v-app-bar>
+		<v-navigation-drawer
+			v-model="drawer"
+			app
+			clipped
+			color="gray lighten-4">
+			<v-list
+				dense
+				class="grey lighten-4 fill-height">			
+				<!-- Calender and filters -->
+			</v-list>
+		</v-navigation-drawer>
+    <v-content>
+    <v-container
+        fluid
+        class="grey lighten-4 fill-height"
+      >
+        <v-row
+          justify="center"
+          align="center"
+        >
+          <v-col class="shrink">
+            <h1>Hello world</h1>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+	</v-app>
+</template>
+
+<script lang="ts">
+	import { Component, Prop, Vue } from 'vue-property-decorator';
+
+	@Component
+	export default class HelloWorld extends Vue {
+		private drawer: boolean = true;
+	}
+</script>
+
+
+<style>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Made with &lt;3 by Snehal Baghel and Vaishanvi Singh. Hopefully..</h1>
   </div>
 </template>
