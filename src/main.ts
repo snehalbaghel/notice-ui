be72244393850c770aa9bb834befb3b5f24bc633
Snehal Baghel <snehalbaghel@gmail.com>
2019-09-06 17:20:02 +0530

    sidbar components

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
+declare module 'v-calendar';
+/// <reference path='./v-calendar.d.ts' />
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import VCalendar from 'v-calendar';
 
 Vue.config.productionTip = false;
+
+Vue.use(VCalendar, {
+  componentPrefix: 'vc',  // Use <vc-calendar /> instead of <v-calendar />
+});
 
 new Vue({
   router,
