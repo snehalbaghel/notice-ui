bb85728bea780ace989b2ba0f5744f15391d81ed
Vaishnavi Singh <vaishnavisingh.1509@gmail.com>
2019-09-16 20:20:47 +0530

    buttons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <Admin />
+    <!--Admin /-->
     <!--Student /-->
     <!--Status /-->
     <!--AppHeader /-->
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,7 +15,8 @@
 				label="Search"
 				prepend-inner-icon="search"
 			/>
-        <v-btn class="butt" color="teal" dark text><font color="white">Upcoming</font></v-btn><v-divider vertical></v-divider>
+			
+        <!--v-btn class="butt" color="teal" dark text><font color="white">Upcoming</font></v-btn><v-divider vertical></v-divider-->
 			<div class="flex-grow-1"></div>
 			
 		</v-app-bar>
